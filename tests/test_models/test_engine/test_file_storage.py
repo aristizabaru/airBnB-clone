cf6bae10739f3b6f163ab0af570db1587689c45a
Andres Aristizabal <andres.aristizabal@gmail.com>
2021-02-16 22:35:17 +0000

    Test(test_storage_all): Test that storage returns the FileStorage.__objects attr

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,6 +2,7 @@
 """test_file_storage module """
 import unittest
 import pep8
+import models.engine.file_storage as engine
 
 
 class TestFileStorageDocs(unittest.TestCase):
@@ -13,3 +14,15 @@ class TestFileStorageDocs(unittest.TestCase):
         result = pep8s.check_files(['models/engine/file_storage.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
+
+
+class TestFileStorage(unittest.TestCase):
+    """Test the FileStorage class"""
+
+    def test_all_returns_dict(self):
+        """Test that all returns the FileStorage.__objects attr"""
+        storage = engine.FileStorage()
+        new_dict = storage.all()
+        self.assertEqual(type(new_dict), dict)
+        # Take name of private attribute from __dict__
+        self.assertIs(new_dict, storage._FileStorage__objects)
