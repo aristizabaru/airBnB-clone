8761ef4dc67e48cbf4b369b8fe5021f06e5b7df1
Andres Aristizabal <andres.aristizabal@gmail.com>
2021-02-16 21:01:16 +0000

    Test(test_to_dict): Test conversion of object attributes to dictionary for json

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -25,3 +25,18 @@ class TestBaseModel(unittest.TestCase):
         inst = BaseModel()
         string = "[BaseModel] ({}) {}".format(inst.id, inst.__dict__)
         self.assertEqual(string, str(inst))
+
+    def test_to_dict(self):
+        """Test conversion of object attributes to dictionary for json"""
+        my_base = BaseModel()
+        my_base.name = "Betty"
+        my_base.my_number = 60
+        my_base.email = "betty.holberton@airbnb.com"
+        base_dict = my_base.to_dict()
+        attributes = ["id", "created_at", "updated_at",
+                      "name", "my_number", "email", "__class__"]
+        self.assertCountEqual(base_dict.keys(), attributes)
+        self.assertEqual(base_dict["__class__"], "BaseModel")
+        self.assertEqual(base_dict["name"], "Betty")
+        self.assertEqual(base_dict["my_number"], 60)
+        self.assertEqual(base_dict["email"], "betty.holberton@airbnb.com")
