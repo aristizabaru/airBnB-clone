867c67053362c467eaf200155482134d217868ba
Andres Aristizabal <andres.aristizabal@gmail.com>
2021-02-17 03:24:17 +0000

    Test(missing class): add missing class test for all commands

--- tests/test_console.py
+++ tests/test_console.py
@@ -18,12 +18,55 @@ class TestConsoleDocs(unittest.TestCase):
                          "Found code style errors (and warnings).")
 
 
-class Test_destroy(unittest.TestCase):
+class TestModels(unittest.TestCase):
+    def test_create_invalid_class(self):
+        """  Test for create with invalid class """
+        msg = "** class doesn't exist **\n"
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("create MyModel")
+            st = f.getvalue()
+            self.assertEqual(msg, st)
+
+    def test_show_invalid_class(self):
+        """  Test for show non-existent class """
+        msg = "** class doesn't exist **\n"
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("show MyModel")
+            st = f.getvalue()
+            self.assertEqual(msg, st)
+
+     def test_all_invalid_class(self):
+        """Test for all with invalid class"""
+        msg = "** class doesn't exist **\n"
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            pre_cmd = HBNBCommand().precmd("MyModel.all()")
+            HBNBCommand().onecmd(pre_cmd)
+            st = f.getvalue()
+            if st[0] == "\n":
+                msg = "\n" + msg
+            self.assertEqual(msg, st)
+
     def test_destroy_invalid_class(self):
-        """  Test for destroy with invalid class """
+        """Test for destroy with invalid class"""
         msg = "** class doesn't exist **\n"
 
         with patch('sys.stdout', new=io.StringIO()) as f:
             HBNBCommand().onecmd("destroy MyModel")
+            st = f.getvalue()
+            self.assertEqual(msg, st)
+    
+     def test_all_invalid_class(self):
+        """  Test for all with no existent class """
+        msg = "** class doesn't exist **\n"
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("all MyModel")
+            st = f.getvalue()
+            self.assertEqual(msg, st)
+
+    def test_update_invalid_class(self):
+        """  Test for update with no existent class """
+        msg = "** class doesn't exist **\n"
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("update MyModel")
             st = f.getvalue()
             self.assertEqual(msg, st)
