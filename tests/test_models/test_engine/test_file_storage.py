0ddae113fe574c3c5e85678096fa2cabd9fb0be1
Andres Aristizabal <andres.aristizabal@gmail.com>
2021-02-17 02:21:57 +0000

    Test: add test for new instances and reload from json

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -6,6 +6,7 @@ import models.engine.file_storage as engine
 import json
 import models.base_model as base
 import os
+import models
 
 
 class TestFileStorageDocs(unittest.TestCase):
@@ -78,3 +79,71 @@ class TestFileStorage(unittest.TestCase):
         pwd = os.getcwd()
         path = pwd + "/file.json"
         self.assertTrue(os.path.exists(path))
+
+    def test_new(self):
+        """test new instance in objects"""
+
+        # Create BaseModel instance.
+        base_x = base.BaseModel()
+
+        with self.assertRaises(TypeError):
+            models.storage.new(base_x, 1)
+        tempo_dict = models.storage.all()
+
+        # Concatenate ClassName.id string
+        base_x_id = "BaseModel" + "." + base_x.id
+
+        # Set attributes.
+        user_x = models.user.User()
+        user_x.email = "pruebas@prueba.com"
+        user_x.first_name = "Don"
+        user_x.last_name = "Pruebas"
+        user_x.password = "tambienprueba"
+
+        user_x_id = "User." + user_x.id
+
+        for key, value in tempo_dict.items():
+            if key == user_x_id:
+                self.assertTrue(hasattr(user_x, "email"))
+                self.assertTrue(hasattr(user_x, "first_name"))
+                self.assertTrue(hasattr(user_x, "last_name"))
+                self.assertTrue(hasattr(user_x, "password"))
+                self.assertTrue(hasattr(user_x, "id"))
+
+        self.assertIn(base_x_id, tempo_dict.keys())
+
+        self.assertIn(user_x_id, tempo_dict.keys())
+
+        models.storage.save()
+        # Test file created after new
+        pwd = os.getcwd()
+        path = pwd + "/file.json"
+        self.assertTrue(os.path.exists(path))
+
+    def test_reload(self):
+        """Test fors the reload() method"""
+
+        # Create test instances.
+        bas = models.base_model.BaseModel()
+        ame = models.amenity.Amenity()
+        rev = models.review.Review()
+        pla = models.place.Place()
+        sta = models.state.State()
+        usr = models.user.User()
+        cit = models.city.City()
+
+        # Create list of objects.
+        list_objs = [bas, ame, rev, pla, sta, usr, cit]
+
+        list_obj_id = []
+        for obj in list_objs:
+            list_obj_id.append(obj.__class__.__name__ + "." + obj.id)
+
+        # Save objects into file.
+        models.storage.save()
+
+        # Start test for reload.
+        models.storage.reload()
+
+        for obj_id in list_obj_id:
+            self.assertIn(obj_id, models.storage.all().keys())
