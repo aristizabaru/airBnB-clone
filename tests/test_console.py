ae0982e24efbfc6a1a8f13e51a495e3eeed53084
Andres Aristizabal <andres.aristizabal@gmail.com>
2021-02-17 03:13:16 +0000

    Test(test_destroy_invalid_class): Test for destroy with invalid class

--- tests/test_console.py
+++ tests/test_console.py
@@ -2,6 +2,9 @@
 """test_console module """
 import unittest
 import pep8
+import io
+from unittest.mock import patch
+from console import HBNBCommand
 
 
 class TestConsoleDocs(unittest.TestCase):
@@ -13,3 +16,14 @@ class TestConsoleDocs(unittest.TestCase):
         result = pep8s.check_files(['console.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
+
+
+class Test_destroy(unittest.TestCase):
+    def test_destroy_invalid_class(self):
+        """  Test for destroy with invalid class """
+        msg = "** class doesn't exist **\n"
+
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd("destroy MyModel")
+            st = f.getvalue()
+            self.assertEqual(msg, st)
